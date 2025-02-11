87eef247c47426a51ef6bf47d39980e312f6f27f
Yan Ren <yan.ren@lightspeedhq.com>
2025-02-10 19:15:24 -0500

    Add class note

---
+++ class0208.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    vector<int> give(4);
+
+    for(int i = 0; i < give.size(); i++) {
+        cin >> give[i];
+    }
+    
+    return 0;
+}
